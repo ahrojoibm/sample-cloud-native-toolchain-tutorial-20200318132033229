052939b248f983e4d1fd6b3c0212f44e0b8ad379
Andrés Iván Huerta Rojo <Andres.Huerta@ibm.com>
2020-03-18 16:44:29 +0000

    asdfgh

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -20,7 +20,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello, stranger!');
+            res.body.should.eql('Well hello there, stranger!');
 
             done();
           });
@@ -39,7 +39,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello, Scott!');
+            res.body.should.eql('Well hello there, Scott!');
 
             done();
           });
